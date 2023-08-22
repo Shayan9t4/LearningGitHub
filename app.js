e876d9bb831ca07e92e504c897ca41caba9cd360
Shayan9t4 <Shayan9t4@gmail.com>
2023-08-22 08:19:04 -0700

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+// var num = Math.random();
+// var convertedNum = Math.round(num);
+// if (convertedNum == 0) {
+//   console.log("Head");
+// } else {
+//   console.log("Tail");
+// }
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+
+// var date = new Date();
+// console.log(date);
+// var months = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "June",
+//   "July",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+// var getMonth = date.getMonth();
+// console.log(months[getMonth]);
+
